cc5ccf2126cd2d6da3c3989b1b0f0a64088d82e3
Juwon-Kiim <kimjuwon@gimjuwon-ui-MacBookAir.local>
2024-03-06 22:27:08 +0900

    setting csrf enable

--- TestSecurity/src/main/java/com/example/TestSecurity/config/SecurityConfig.java
+++ TestSecurity/src/main/java/com/example/TestSecurity/config/SecurityConfig.java
@@ -20,7 +20,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(AbstractHttpConfigurer::disable)
+                //.csrf(AbstractHttpConfigurer::disable)
                 .formLogin(login -> login
                         .loginPage("/login")
                         .loginProcessingUrl("/loginProc"))
