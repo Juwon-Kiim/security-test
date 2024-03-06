a45fab26d3dd4a4cd1cd0cfd45295bd641fb410d
Juwon-Kiim <kimjuwon@gimjuwon-ui-MacBookAir.local>
2024-03-07 00:13:59 +0900

    setting Role Hierarchy

--- TestSecurity/src/main/java/com/example/TestSecurity/config/SecurityConfig.java
+++ TestSecurity/src/main/java/com/example/TestSecurity/config/SecurityConfig.java
@@ -1,7 +1,10 @@
 package com.example.TestSecurity.config;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -22,26 +25,13 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(AbstractHttpConfigurer::disable)
-                .formLogin(login -> login
-                        .loginPage("/login")
-                        .loginProcessingUrl("/loginProc"))
-//                .httpBasic(Customizer.withDefaults())
-                .sessionManagement(auth -> auth
-                        .sessionFixation().changeSessionId()
-                        .maximumSessions(1)
-                        .maxSessionsPreventsLogin(true))
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/", "/login", "/join", "/joinProc").permitAll()
-                        .requestMatchers("/admin").hasRole("ADMIN")
-                        .requestMatchers("/my/**").hasAnyRole("ADMIN", "USER")
-                        .anyRequest().authenticated());
-
-        return http.build();
-    }
+//    @Bean
+//    public RoleHierarchy roleHierarchy(){
+//        RoleHierarchyImpl hierarchy = new RoleHierarchyImpl();
+//        hierarchy.setHierarchy("ROLE_ADMIN > ROLE_MANAGER\n" +
+//                "ROLE_MANAGER > ROLE_USER");
+//        return hierarchy;
+//    }
 
 //    @Bean
 //    public UserDetailsService userDetailsService(){
@@ -53,9 +43,36 @@ public class SecurityConfig {
 //        UserDetails user2 = User.builder()
 //                .username("user2")
 //                .password(bCryptPasswordEncoder().encode("1234"))
-//                .roles("ADMIN")
+//                .roles("USER")
 //                .build();
 //
 //        return new InMemoryUserDetailsManager(user1, user2);
 //    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+//                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(login -> login
+                        .loginPage("/login")
+                        .loginProcessingUrl("/loginProc").permitAll())
+//                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(auth -> auth
+                        .sessionFixation().changeSessionId()
+                        .maximumSessions(1)
+                        .maxSessionsPreventsLogin(true))
+//                .authorizeHttpRequests(auth -> auth
+//                        .requestMatchers("/login").permitAll()
+//                        .requestMatchers("/").hasAnyRole("USER")
+//                        .requestMatchers("/manager").hasAnyRole("MANAGER")
+//                        .requestMatchers("/admin").hasAnyRole("ADMIN")
+//                        .anyRequest().authenticated());
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/", "/login", "/join", "/joinProc").permitAll()
+                        .requestMatchers("/admin").hasRole("ADMIN")
+                        .requestMatchers("/my/**").hasAnyRole("ADMIN", "USER")
+                        .anyRequest().authenticated());
+
+        return http.build();
+    }
 }
