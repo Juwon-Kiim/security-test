bd2d076f30777403c12eba47897151a37bc8c0db
Juwon-Kiim <kimjuwon@gimjuwon-ui-MacBookAir.local>
2024-03-06 22:41:43 +0900

    setting httpBasic

--- TestSecurity/src/main/java/com/example/TestSecurity/config/SecurityConfig.java
+++ TestSecurity/src/main/java/com/example/TestSecurity/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.TestSecurity.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -24,10 +25,11 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                //.csrf(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
                 .formLogin(login -> login
                         .loginPage("/login")
                         .loginProcessingUrl("/loginProc"))
+//                .httpBasic(Customizer.withDefaults())
                 .sessionManagement(auth -> auth
                         .sessionFixation().changeSessionId()
                         .maximumSessions(1)
