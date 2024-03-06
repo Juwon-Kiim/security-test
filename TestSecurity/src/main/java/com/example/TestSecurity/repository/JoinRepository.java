6d8128db1a7a025c037e799a09cae67038920d26
Juwon-Kiim <kimjuwon@gimjuwon-ui-MacBookAir.local>
2024-03-06 18:12:58 +0900

    develop join logic

---
+++ TestSecurity/src/main/java/com/example/TestSecurity/repository/JoinRepository.java
@@ -0,0 +1,8 @@
+package com.example.TestSecurity.repository;
+
+import com.example.TestSecurity.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JoinRepository extends JpaRepository<UserEntity, Long> {
+    UserEntity findByUsername(String username);
+}
---
+++ TestSecurity/src/main/java/com/example/TestSecurity/service/JoinService.java
@@ -0,0 +1,34 @@
+package com.example.TestSecurity.service;
+
+import com.example.TestSecurity.controller.JoinController;
+import com.example.TestSecurity.dto.JoinDTO;
+import com.example.TestSecurity.entity.UserEntity;
+import com.example.TestSecurity.repository.JoinRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JoinService {
+    private final JoinRepository joinRepository;
+
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public JoinService(JoinRepository joinRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.joinRepository = joinRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    public void joinProcess(JoinDTO joinDTO){
+
+        UserEntity findUsername = joinRepository.findByUsername(joinDTO.getUsername());
+
+        if(findUsername==null) {
+            UserEntity userEntity = new UserEntity();
+            userEntity.setUsername(joinDTO.getUsername());
+            userEntity.setPassword(bCryptPasswordEncoder.encode(joinDTO.getPassword()));
+            userEntity.setRole("ROLE_USER");
+
+            joinRepository.save(userEntity);
+        }
+    }
+}
